4a869934faf6fb798a5b45f5b35be994eb1a6af5
rajatbeladiya <rajatbeladiya7@gmail.com>
2020-01-17 23:43:19 +0530

    Updates

---
+++ precache-manifest.846aa66f050876cede7fc128043c033e.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "403780c740f2600287421b848fdba997",
+    "url": "/monsters-relodex/index.html"
+  },
+  {
+    "revision": "93b1258368e00a72bc80",
+    "url": "/monsters-relodex/static/css/main.d0510426.chunk.css"
+  },
+  {
+    "revision": "e7c38e8264c152a28725",
+    "url": "/monsters-relodex/static/js/2.6ab20610.chunk.js"
+  },
+  {
+    "revision": "d705cb622423d72c5defbf368ca70dcc",
+    "url": "/monsters-relodex/static/js/2.6ab20610.chunk.js.LICENSE"
+  },
+  {
+    "revision": "93b1258368e00a72bc80",
+    "url": "/monsters-relodex/static/js/main.d43da138.chunk.js"
+  },
+  {
+    "revision": "681fdffcd020563499b4",
+    "url": "/monsters-relodex/static/js/runtime-main.58269b4a.js"
+  }
+]);
